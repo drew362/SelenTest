ca300faef9fc3f5f1306bcd9814b587cf9b3cf20
Козин Андрей <a.kozin@sbereducation.ru>
2024-03-15 18:54:43 +0300

    add groovy for jenkins

---
+++ uiTestBuild.groovy
@@ -0,0 +1,23 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                git 'https://github.com/drew362/SelenTest.git'
+            }
+        }
+
+        stage('Build') {
+            steps {
+                sh 'mvn clean test'
+            }
+        }
+
+        stage('Run UI Tests') {
+            steps {
+                sh 'mvn test'
+            }
+        }
+    }
+}
