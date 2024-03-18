7409db8b8619935eb9a3a05d882630972fcc3cf5
Козин Андрей <a.kozin@sbereducation.ru>
2024-03-18 14:33:33 +0300

    add groovy for jenkins

--- jenkins.groovy
+++ jenkins.groovy
@@ -82,7 +82,7 @@ def generateAllure() {
             jdk              : '',
             properties       : [],
             reportBuildPolicy: 'ALWAYS',
-            results          : [[path: 'build/allure-results']]
+            results          : [[path: 'привет']]
     ])
 }
 
