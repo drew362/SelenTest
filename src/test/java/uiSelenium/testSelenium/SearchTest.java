8e3810351dfeb3b8081764976d732091624fcc91
Козин Андрей <a.kozin@sbereducation.ru>
2024-03-20 12:44:14 +0300

    edit allure report

---
+++ src/test/java/uiSelenium/testSelenium/SearchTest.java
@@ -0,0 +1,13 @@
+//package uiSelenium.testSelenium;
+//
+//
+//import org.junit.Test;
+//import uiSelenium.BaseSeleniumTest;
+//
+//public class SearchTest extends BaseSeleniumTest {
+//
+//    @Test
+//    public void checkSearchTest() {
+////            driver.get("http://localhost:8086/search");
+//    }
+//}
