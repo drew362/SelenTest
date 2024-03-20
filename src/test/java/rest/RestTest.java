165599907fbd09d9fe9f3729f0ed3e34555d3b79
Козин Андрей <a.kozin@sbereducation.ru>
2024-03-20 16:59:48 +0300

    edit allure report

--- src/test/java/rest/RestTest.java
+++ src/test/java/rest/RestTest.java
@@ -1,19 +1,25 @@
 package rest;
 
 import io.qameta.allure.Feature;
+import io.qameta.allure.Features;
+import io.qameta.allure.Stories;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 import static io.restassured.RestAssured.given;
 
+@Feature("fg")
+//@Stories("fgd")
 public class RestTest {
 
     @Feature("Login")
+
     @Test
     public void searchTest() {
         given().
                 baseUri("http://localhost:8086/").
                 when().log().all().
                 get("/search?title=200151").
-                then().statusCode(200);
+                then().statusCode(300);
     }
 }
